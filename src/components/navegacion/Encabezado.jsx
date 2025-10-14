94b5026f9f41459ecdd6231d9c029988b2cfc8af
VilyVargas <vily05dominic23@gmail.com>
2025-10-14 11:03:34 -0600

    Iconos, network, encabezado

--- src/components/navegacion/Encabezado.jsx
+++ src/components/navegacion/Encabezado.jsx
@@ -31,28 +31,85 @@ return (
       <Navbar.Toggle
         aria-controls="menu-offcanvas"
         onClick={manejarToggle}
-        className="bg-light"
+        className="bg-primary"
       />
       <Navbar.Offcanvas
         id="menu-offcanvas"
         placement="end"
         show={mostrarMenu}
         onHide={() => setMostrarMenu(false)}
+        className="bg-white"
+        
       >
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Menu principal</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           <Nav className="flex-grow-1 pe-3">
-            <Nav.Link onClick={() => manejarNavegacion("/")}>Inicio</Nav.Link>
-            <Nav.Link onClick={() => manejarNavegacion("/categorias")}>Categorías</Nav.Link>
-            <Nav.Link onClick={() => manejarNavegacion("/usuarios")}>Usuarios</Nav.Link>
-            <Nav.Link onClick={() => manejarNavegacion("/empleados")}>Empleados</Nav.Link>
-            <Nav.Link onClick={() => manejarNavegacion("/clientes")}>Clientes</Nav.Link>
-            <Nav.Link onClick={() => manejarNavegacion("/compras")}>Compras</Nav.Link>
-            <Nav.Link onClick={() => manejarNavegacion("/ventas")}>Ventas</Nav.Link>
-            <Nav.Link onClick={() => manejarNavegacion("/productos")}>Productos</Nav.Link>
-            <Nav.Link onClick={() => manejarNavegacion("/catalogo")}>Catalogo</Nav.Link>
+
+            <Nav.Link 
+              className={mostrarMenu ? "color-favorito" : "text-white"}
+              onClick={() => manejarNavegacion("/")}
+            >
+              {mostrarMenu ? <i className="bi-house-fill me-2"> </i> : null} Inicio
+              </Nav.Link>
+
+            <Nav.Link
+              className={mostrarMenu ? "color-favorito" : "text-white"} 
+              onClick={() => manejarNavegacion("/categorias")}
+            >
+              {mostrarMenu ? <i className="bi-bookmark-plus me-2"> </i> : null} Categorías
+            </Nav.Link>
+
+            <Nav.Link
+              className={mostrarMenu ? "color-favorito" : "text-white"}
+              onClick={() => manejarNavegacion("/usuarios")}
+            >
+              {mostrarMenu ? <i className="bi-person-vcard-fill me-2"> </i> : null} Usuarios
+            </Nav.Link>
+
+            <Nav.Link 
+              className={mostrarMenu ? "color-favorito" : "text-white"}
+              onClick={() => manejarNavegacion("/empleados")}
+            >
+              {mostrarMenu ? <i className="bi-person-badge-fill me-2"> </i> : null} Empleados
+            </Nav.Link>
+
+            <Nav.Link 
+              className={mostrarMenu ? "color-favorito" : "text-white"}
+              onClick={() => manejarNavegacion("/clientes")}
+            >
+              {mostrarMenu ? <i className="bi-person-circle me-2"> </i> : null} Clientes
+            </Nav.Link>
+
+            <Nav.Link
+              className={mostrarMenu ? "color-favorito" : "text-white"}
+              onClick={() => manejarNavegacion("/compras")}
+            >
+              {mostrarMenu ? <i className="bi-cart-fill me-2"> </i> : null} Compras
+            </Nav.Link>
+
+            <Nav.Link 
+              className={mostrarMenu ? "color-favorito" : "text-white"}
+              onClick={() => manejarNavegacion("/ventas")}
+            >
+              {mostrarMenu ? <i className="bi-cash-coin me-2"> </i> : null} Ventas
+            </Nav.Link>
+
+            <Nav.Link
+              className={mostrarMenu ? "color-favorito" : "text-white"}
+              onClick={() => manejarNavegacion("/productos")}
+            >
+              {mostrarMenu ? <i className="bi-box-fill me-2"> </i> : null}  Productos
+            </Nav.Link>
+
+            <Nav.Link 
+              className={mostrarMenu ? "color-favorito" : "text-white"}
+              onClick={() => manejarNavegacion("/catalogo")}
+            >
+              {mostrarMenu ? <i className="bi-images me-2"> </i> : null} Catalogo
+            </Nav.Link>
+
           </Nav>
         </Offcanvas.Body>
       </Navbar.Offcanvas>
